70b58f8fa1b0b97fccb01ef8dae43277ff574ed6
shaikhsaad7 <ssaadshaikh411@gmail.com>
2025-02-16 13:08:38 +0530

    Add files via upload

---
+++ file-name-1 (3).py
@@ -0,0 +1,14 @@
+num1=int(input( "Enter the first number:"))
+num2=int(input("Enter the second number:"))
+sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+rdr=num1%num2
+flt=num1//num2
+print ("The sum of two numbers is:",sum)
+print ("The difference of two numbers is:",sub)
+print ("The product of two numbers is:",mul)
+print ("The quotient of two numbers is:",div)
+print ("The remainder of the numbers is:",rdr)
+print ("The float value of the numbers is:",flt)
